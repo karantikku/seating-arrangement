7c0bcebeb8b2df130e0c61994619017b26dc9335
karantikku <16414052+karantikku@users.noreply.github.com>
2018-04-02 12:00:08 +0530

    Update seatingArrangement.cpp

--- seatingArrangement.cpp
+++ seatingArrangement.cpp
@@ -3,66 +3,65 @@
 #include <vector>
 #include <math.h>
 using namespace std;
-int main()
+int
+main ()
 {
-    int i,weeks,j,totalPeople,groupSize,p,numberOfGroups,group;
-    cout<<"Enter Strength";
-    cin>>totalPeople;
-    cout<<"Enter group Size";
-    cin>>groupSize;
-    numberOfGroups=totalPeople/groupSize;
-    int FactorCount = 0;
-	for (i=numberOfGroups; i>=2; --i)
+  int i, weeks, j, totalPeople, groupSize, p, m, group, noOfGroups;
+  cout << "Enter Strength";
+  cin >> totalPeople;
+  cout << "Enter group Size";
+  cin >> groupSize;
+  m = totalPeople / groupSize;
+  int FactorCount = 0;
+  for (i = m; i >= 2; --i)
+    {
+      for (j = 2; j < i; ++j)
 	{
-		for (j=2; j<i; ++j)
-		{
-			if (i % j == 0)
-			{
-				++FactorCount;
-			}
-		}
-		if (FactorCount == 0)
-		{
-			p=i;
-			break;
-		}
-		FactorCount = 0;
+	  if (i % j == 0)
+	    {
+	      ++FactorCount;
+	    }
 	}
-	int c=0;
-	char arr[groupSize][numberOfGroups];
-	for(i=0;i<groupSize;i++)
-    {
-        for(j=0;j<numberOfGroups;j++)
-        {
-            arr[i][j]= 65 + c;
-            c++;
-        }
+      if (FactorCount == 0)
+	{
+	  noOfGroups = i;
+	  break;
+	}
+      FactorCount = 0;
     }
-    cout<<endl;
-    int temp;
-    string weeksArrangement[p][p];
-    for(weeks=0;weeks<p;weeks++)
+  int c = 0;
+  char arr[groupSize][m];
+  for (i = 0; i < groupSize; i++)
     {
-    for(group=0;group<p;group++)
-        {
-            string x;
-      for(j=0;j<groupSize;j++)
-           {
-               temp = ((weeks*j) + group)%p;
-               char d;
-               d = arr[j][temp];
-               x = x+d;
-            }
-            weeksArrangement[weeks][group] = weeksArrangement[weeks][group]+x;
-        }
+      for (j = 0; j < m; j++)
+	{
+	  arr[i][j] = 65 + c;
+	  c++;
+	}
     }
-    for(i=0;i<p;i++)
+  cout << endl;
+  int temp;
+  string weeksArrangement[noOfGroups][noOfGroups];
+  for (weeks = 0; weeks < noOfGroups; weeks++)
     {
-        cout<<endl;
-        for(j=0;j<p;j++)
-        {
-            cout<<weeksArrangement[i][j]<<"  ";
-        }
+      for (group = 0; group < noOfGroups; group++)
+	{
+	  string x;
+	  for (j = 0; j < groupSize; j++)
+	    {
+	      temp = ((weeks * j) + group) % noOfGroups;
+	      x = x + arr[j][temp]
+	    }
+	  weeksArrangement[weeks][group] = weeksArrangement[weeks][group] + x;
+	}
     }
-    return 0;
+  for (i = 0; i < noOfGroups; i++)
+    {
+      cout << endl;
+      for (j = 0; j < noOfGroups; j++)
+	{
+	  cout << weeksArrangement[i][j] << "  ";
+	}
+    }
+  return 0;
 }
